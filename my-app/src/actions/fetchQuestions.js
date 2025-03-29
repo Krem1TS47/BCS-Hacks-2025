ae55710b8d619d9fb799e5d41a4aac1899de7c04
Zhong <zhtgeneral@gmail.com>
2025-03-29 13:35:39 -0700

    added missing files. Added insertQuestion

--- my-app/src/actions/fetchQuestions.js
+++ my-app/src/actions/fetchQuestions.js
@@ -3,6 +3,7 @@ import { supabase } from '../lib/Supabase'
 export async function fetchQuestions() {
   const { data, error } = await supabase.from("questions").select("*");
   if (error) {
+    console.log("Fetch question error: " + JSON.stringify(error, null, 2));
     return null;
   }
   return data;
